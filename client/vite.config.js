fb7329b9c60ca276658bc4fda1c8fdaeb2b4c39a
elcodo254 <ellawachaga@gmail.com>
2023-10-18 22:48:14 -0700

    vite configuration for server connection

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.jsx',
+    },
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:3001/", // the address that u serve in the backend 
+    },
+  },
+}
