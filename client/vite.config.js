30395906f2b61b997db06765bc55f4af99391794
elcodo254 <ellawachaga@gmail.com>
2023-10-18 22:53:01 -0700

    refractor

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: './src/main.jsx',
-    },
-  },
   server: {
     proxy: {
-      "/api": "http://localhost:3001/", // the address that u serve in the backend 
+      '/server': {
+        target: 'http://localhost:3001',
+        secure: false,
+      },
     },
   },
-}
+  plugins: [react()],
+});
+
