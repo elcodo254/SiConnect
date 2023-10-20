93111b6f48254c996188b4ad6f508aa9e6d5d11e
elcodo254 <113864650+elcodo254@users.noreply.github.com>
2023-10-20 09:47:58 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ import { users, posts } from "./data/index.js";
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const __dirName = path.resolve();
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -53,9 +54,9 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 /* DEPLOYMENT SETUP */
-app.use(express.static(path.join(__dirname, "client/dist")))
+app.use(express.static(path.join(__dirName, "client/dist")))
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirName, 'client', 'dist', 'index.html'));
 });
 
 /* MONGOOSE SETUP */
