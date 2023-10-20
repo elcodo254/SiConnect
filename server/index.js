5500146428c6bc53b7279234bbd060f9eb3bfc0b
elcodo254 <113864650+elcodo254@users.noreply.github.com>
2023-10-20 05:45:16 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-app.use("/client", express.static(path.join(__dirname, "/client/dist")))
+app.use("/client", express.static(path.join(__dirname, "client/dist")))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
