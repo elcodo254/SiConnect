57075fe91b53f610fdd32d04f0e579222659f8d9
elcodo254 <113864650+elcodo254@users.noreply.github.com>
2023-10-20 08:59:09 +0300

    Update index.js
    configurations to fix path issues on deployment

--- server/index.js
+++ server/index.js
@@ -31,10 +31,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-app.use("/client", express.static(path.join(__dirname, "client/dist")))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
@@ -55,6 +51,12 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+
+/* DEPLOYMENT SETUP */
+app.use(express.static(path.join(__dirname, "client/dist")))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
