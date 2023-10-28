f29d0e8f60b4544ccde8838701f76a3727f0999c
elcodo254 <ellawachaga@gmail.com>
2023-10-28 15:19:10 -0700

    refactor

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -1,5 +1,4 @@
 import { Typography, useTheme } from "@mui/material";
-import { useSelector } from "react-redux";
 import FlexBetween from "../../components/FlexBetween";
 import WidgetWrapper from "../../components/WidgetWrapper";
 
@@ -22,7 +21,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src=`${URL}/info4.jpeg`
+        src="http://localhost:3001/assets/info4.jpeg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
