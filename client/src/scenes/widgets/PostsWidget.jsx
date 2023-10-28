adf92b8a3be98fa833f87104f95dc3ebdc2eb7a1
elcodo254 <ellawachaga@gmail.com>
2023-10-28 15:13:05 -0700

    update PostsWidget.jsx:fix error

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -1,4 +1,4 @@
-mport { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "../../state";
 import PostWidget from "./PostWidget";
