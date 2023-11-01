98945954d796a3bd4855601ce4d69fc29cbb5e65
elcodo254 <113864650+elcodo254@users.noreply.github.com>
2023-11-01 15:19:44 +0300

    Update UserImage.jsx

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`/server/assets/${image}`}
+        src={`/assets/${image}`}
       />
     </Box>
   );
